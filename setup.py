7d12d1bf8d5558198e1d27bd66372601c26ca6ed
monteemc <monteemc@users.noreply.github.com>
2019-04-30 22:48:58 -0300

    Package setup correctd

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/monteemc/fpyhelper",
     packages=setuptools.find_packages(),
+    py_modules=["fpyhelper"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
